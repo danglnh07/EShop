6f088459eaf9a4f43c6e4c61cc010d7b58ac74b4
danglnh07 <danglnh07.work@gmail.com>
2026-04-02 15:44:00 +0700

    Fix error in Profiles/Profile.cs: remove the mapping config for GetProductByCategory

--- src/Services/Catalog/Catalog.API/Profiles/Profile.cs
+++ src/Services/Catalog/Catalog.API/Profiles/Profile.cs
@@ -1,5 +1,4 @@
 using Catalog.API.Products.GetProducts;
-using Catalog.API.Products.GetProductsByCategory;
 using Mapster;
 
 namespace Catalog.API.Profiles
@@ -9,8 +8,6 @@ namespace Catalog.API.Profiles
         public void Register(TypeAdapterConfig config)
         {
             config.NewConfig<GetProductsResult, GetProductsResponse>()
-                .MapWith(src => new(src.Products, new(src.Products.HasNextPage, src.Products.HasPreviousPage, src.Products.TotalItemCount, src.Products.PageSize, src.Products.PageNumber)));
-            config.NewConfig<GetProductsByCategoryResult, GetProductsByCategoriesResponse>()
                 .MapWith(src => new(src.Products, new(src.Products.HasNextPage, src.Products.HasPreviousPage, src.Products.TotalItemCount, src.Products.PageSize, src.Products.PageNumber)));
         }
     }
